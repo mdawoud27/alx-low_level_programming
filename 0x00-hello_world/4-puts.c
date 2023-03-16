36a6e5f101b93d0ad703c1e789003559a59f066d
dawoud <mhmaddawoud20@gmail.com>
2023-03-16 14:01:23 -0700

    Update the file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
