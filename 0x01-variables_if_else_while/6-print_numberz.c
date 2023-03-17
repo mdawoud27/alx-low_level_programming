9de15b99f80bde7014c15afa7a884f471a0fc43f
dawoud <mhmaddawoud20@gmail.com>
2023-03-17 15:01:41 -0700

    Adding 6-print_numberz.c file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,8 @@ int main(void)
 {
         int i;
 
-        for (i = 0; i < 10; i++)
-                printf("%d", i);
+        for (i = '0'; i <= '9'; i++)
+                printf("%c", i);
         printf("\n");
         return (0);
 }
