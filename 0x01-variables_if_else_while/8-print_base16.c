a4d156276d3da2557a4d1f8eba4834f1b09e1835
dawoud <mhmaddawoud20@gmail.com>
2023-03-17 15:25:11 -0700

    Adding 7-printfile

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * numbers of base 16 in lowercase
+ * Return: 0 always wins :)
+ */
+
+int main(void)
+{
+	printf("0123456789abcdef\n");
+	return (0);
+}
