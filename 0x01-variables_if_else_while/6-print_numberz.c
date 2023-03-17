2a756ac1c2d16a621e9dcf359bc4055c654042cb
dawoud <mhmaddawoud20@gmail.com>
2023-03-17 14:57:20 -0700

    Adding 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * program prints numbers base 10
+ * Return: 0 always wins
+ */
+
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+                printf("%d", i);
+        printf("\n");
+        return (0);
+}
