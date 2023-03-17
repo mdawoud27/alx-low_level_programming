1d479deefef8475b717628dff5926d1f2dc3fdf0
dawoud <mhmaddawoud20@gmail.com>
2023-03-17 13:57:07 -0700

    Adding the third line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entery Point
+ *
+ * Return: 0 always (success)
+ */
+
+int main(void)
+{
+	for (char c = a; c <= z; c++)
+	{
+		printf("%c", c);
+	}
+	printf("\n");
+	return (0);
+}
