bab8674fbba8d6087ffbec2041db230c72b4da43
dawoud <mhmaddawoud20@gmail.com>
2023-03-18 09:35:51 -0700

    Adding 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints all possible different combinations of two digits
+ * Return: 0 always wins
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			if (i < j && i != j)
+			{
+				printf("%d%d", i, j);
+				if (i != 8 && j != 9)
+				{
+					printf(", ");
+				}
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
