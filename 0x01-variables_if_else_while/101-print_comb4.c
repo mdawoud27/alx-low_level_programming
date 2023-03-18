2c94a3af3f886a594e6801d1e16df8a76675e3d9
dawoud <mhmaddawoud20@gmail.com>
2023-03-18 10:25:02 -0700

    Adding 101-print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints all possible different combinations of three digits.
+ * Return: 0 always wins
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 1; j <= 8; j++)
+		{
+			for (k = 2; k <= 9; k++)
+			{
+				if ((i != j && j != k && k != i) && (j > i && j < k))
+					printf("%d%d%d", i, j, k);
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
