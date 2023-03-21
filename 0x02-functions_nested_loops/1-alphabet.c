d2bd92ab57742283256555798f6e68ccf4d5c806
dawoud <mhmaddawoud20@gmail.com>
2023-03-21 08:00:43 -0700

    Adding 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * print_alphabet - function that prints the alphabet, in lowercase
- * Return: 0 always wins
- */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
-
-/**
  * print_alphabet - function that prints the alphabet, in lowercase
  */
 
