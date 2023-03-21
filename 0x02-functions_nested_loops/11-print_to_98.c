b580651aefef7de4da8530e58af66e2296076848
dawoud <mhmaddawoud20@gmail.com>
2023-03-21 15:05:28 -0700

    Adding 11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,19 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
+		printf("98");
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
+		printf("98");
 	}
-	printf('\n');
+	printf("\n");
 }
