d87c1b47a2b15691c7c07432c2333e0ee56dc665
dawoud <mhmaddawoud20@gmail.com>
2023-03-21 13:10:38 -0700

    Adding 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,24 +12,21 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
+		_putchar('1');
 		_putchar('\n');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
+		_putchar('1');
 		_putchar('\n');
 		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
+		_putchar('0');
 		_putchar('\n');
 		return (0);
 	}
