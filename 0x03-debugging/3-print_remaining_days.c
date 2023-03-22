416bf89912a3ae94a0869d2be5519f861088812c
dawoud <mhmaddawoud20@gmail.com>
2023-03-22 05:16:36 -0700

    Adding 3-print_remaining_days.c file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,11 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/**
-	 * leap year when it can be divided
-	 * by 100 and 400 0r by 4 evenly
-	 */
-	if ((year % 100 == 0 && year % 400 == 0) || !(year % 4 == 0))
+	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
