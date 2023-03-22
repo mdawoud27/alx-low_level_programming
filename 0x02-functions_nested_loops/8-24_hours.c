3eaf1762ad2d38a484a97ff6b8f65d293bbbf61c
dawoud <mhmaddawoud20@gmail.com>
2023-03-22 08:42:35 -0700

    Adding 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,4 +8,21 @@
 
 void jack_bauer(void)
 {
+	int hour, minute;
 
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			if (hour <= 9)
+				printf("0%d:", hour);
+			else
+				printf("%d:", hour);
+			if (minute <= 9)
+				printf("0%d", minute);
+			else
+				printf("%d", minute);
+			printf("\n");
+		}
+	}
+}
