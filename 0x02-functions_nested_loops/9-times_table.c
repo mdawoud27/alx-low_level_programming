868032d9c268d1852c3295fb74b432d0ad17a4b6
dawoud <mhmaddawoud20@gmail.com>
2023-03-22 10:00:22 -0700

    Adding 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * times_table - function that prints the 9 times table, starting with 0.
- * Return: void
+ * times_table - Prints the 9 times table, starting from 0.
  */
-
 void times_table(void)
 {
-	int i, j;
+	int num, mult, prod;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar((i * j) + '0');
 			_putchar(',');
 			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
