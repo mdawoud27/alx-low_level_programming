c1c128b028ae02c420637e5b02524742398bea58
dawoud <mhmaddawoud20@gmail.com>
2023-03-23 04:33:53 -0700

    Adding 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# This is a  0x04-more_functions_nested_loops
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: dawoud
+ * Description: Header file containing declarations for all functions
+ *              used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _islower(int c);
+
+#endif
