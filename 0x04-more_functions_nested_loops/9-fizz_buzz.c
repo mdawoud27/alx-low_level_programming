c14310a54c7dddf960911a8ef6b6fabd535b1b96
dawoud <mhmaddawoud20@gmail.com>
2023-03-23 08:50:01 -0700

    Adding 9-fizz_buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
