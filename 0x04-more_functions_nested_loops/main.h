9fde692719602dc8d2581cb9646e821fa4532c1f
dawoud <mhmaddawoud20@gmail.com>
2023-03-23 05:52:54 -0700

    Adding 5-more_numbers.c file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,7 +13,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers(void);
-
+void print_most_numbers(void;
+void more_numbers(void);
 
 #endif
