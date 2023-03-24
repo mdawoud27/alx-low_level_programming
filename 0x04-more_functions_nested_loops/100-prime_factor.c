612196ec6ae814665cf6022fd18d6a9b1cbc4991
dawoud <mhmaddawoud20@gmail.com>
2023-03-24 08:44:07 -0700

    adding the file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A program that finds and prints the largest prime factor of the number 612852475143
+ * Return: 0 always wins
+ */
+
+int main(void)
+{
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - A function that prints an integer
+ * @n: Input number
+ * Return: the value of variable n
+ */
+
+void print_number(int n)
+{
+
