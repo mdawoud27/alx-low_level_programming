b5194e8268274ce12b331563bd570a8981fbc294
dawoud <mhmaddawoud20@gmail.com>
2023-03-27 13:17:46 -0700

    Adding swap file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that swaps the values of two integers.
+ * @a: An integer variable
+ * @b: An integer variable
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = *a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
