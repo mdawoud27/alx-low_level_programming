a8d07e50630bcc1dcb9f1379f382cacca824542b
dawoud <mhmaddawoud20@gmail.com>
2023-03-27 16:01:11 -0700

    Adding 101-keygen.c file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - Entry point
+ * Return: 0 always wins
+ */
+
+int main(void)
+{
+	int sum;
+	char ch;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		ch = rand() * 128;
+		sum += ch;
+		putchar(ch);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
