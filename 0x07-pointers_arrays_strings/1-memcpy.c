ce347c2934bc2bb27a7701e24ecbf173b99b17d9
dawoud <mhmaddawoud20@gmail.com>
2023-04-03 03:35:33 -0700

    Adding 1-memcpy.c file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memcpy - copy chars
+ * _memcpy - function used to copy chars
  * @dest: where chars copied
  * @src: where chars come
  * @n: number of chars
- * Return: address of return;
+ * Return: *dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -13,8 +13,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		dest[i] = src[i];
-	}
 	return (dest);
 }
