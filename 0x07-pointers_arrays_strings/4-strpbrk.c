0e38e52308e529cb1649011ca8e8d9ea2757a11a
dawoud <mhmaddawoud20@gmail.com>
2023-04-03 03:49:32 -0700

    Adding 4-strpbrk.c file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strpbrk - search char
+ * _strpbrk - function that searches a string for
+ * any of a set of bytes.
  * @s: string
  * @accept: string
  * Return: string
@@ -14,12 +15,8 @@ char *_strpbrk(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
-		{
 			if (s[i] == accept[j])
-			{
 				return (&s[i]);
-			}
-		}
 	}
 	return (0);
 }
