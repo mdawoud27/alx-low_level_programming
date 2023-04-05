f360a089cea3520f21cd61bfb2582b9bae58ddcb
dawoud <mhmaddawoud20@gmail.com>
2023-04-05 12:59:13 -0700

    Adding 6-is_prime_number.c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
- * isPrime - check prime
- * @n: number
- * @i: counter
+ * isPrime - Write a function that 
+ * returns 1 if the input integer is a prime number, 
+ * otherwise return 0
+ * @n: A number
+ * @i: A counter
  * Return: 1 if 0
  */
+
 int isPrime(int n, int i)
 {
 	if (i == 1)
@@ -16,9 +19,10 @@ int isPrime(int n, int i)
 }
 /**
  * is_prime_number - check if the number is prime of not
- * @n: number
+ * @n: A number
  * Return: 1 if true, 0 if not
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
