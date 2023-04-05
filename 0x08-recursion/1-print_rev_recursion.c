f6bf279f5b9f752be09917818634f83f68409ca8
dawoud <mhmaddawoud20@gmail.com>
2023-04-05 12:42:54 -0700

    Adding 1-print_rev_recursion.c file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - print reversed string
+ * _print_rev_recursion - function that prints a string in reverse.
  * @s: sting
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s + 1);
+		_print_rev_recursion(1 + s);
 		_putchar(*s);
 	}
 }
