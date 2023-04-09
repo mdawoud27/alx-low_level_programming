7330543785d61b0825fbb0fceb1f2060de968a7e
dawoud <mhmaddawoud20@gmail.com>
2023-04-09 10:31:18 -0700

    Adding 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,19 +15,13 @@ int main(int argc, char *argv[])
 	int sum = 0, i, j;
 
 	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+		return (printf("0\n"), 0);
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0', j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+				return (printf("Error\n"), 1);
 		}
 		sum += atoi(argv[i]);
 	}
