5cbcf4526a8a1efcb022f56b5be949b17f10c6d2
dawoud <mhmaddawoud20@gmail.com>
2023-04-10 03:56:11 -0700

    Program output file

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * @argc: A number of arguments
+ * @argv: An array of pointer string
+ * Return: 0 always wins
+ */
+
+int main()
