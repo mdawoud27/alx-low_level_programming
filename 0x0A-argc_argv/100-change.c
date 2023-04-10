845abffcd028d9186e1b6321bf7d93bd68d8408f
dawoud <mhmaddawoud20@gmail.com>
2023-04-10 05:33:27 -0700

    100-change.c file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,4 +10,25 @@
  * Return: 0 always wins
  */
 
-int main()
+int main(int argc, char **argv)
+{
+	int cents, coin_cnt = 0, temp = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+		return (printf("Error\n"), 1);
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		if (cents >= coins[temp])
+		{
+			cents -= coins[temp];
+			coin_cnt++;
+		}
+		else
+			temp++;
+	}
+	printf("%d\n", coin_cnt);
+
+	return (0);
+}
