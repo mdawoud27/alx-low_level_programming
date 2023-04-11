bb688b14dadbaa9c74728fdfd30ec46faf0aac6a
dawoud <mhmaddawoud20@gmail.com>
2023-04-11 08:13:39 -0700

    Adding 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int len_s1 = 0, len_s2 = 0, i, j;
 	char *new_str;
 
-	if (s1 == NULL)
-		len_s2 = strlen(s2);
-	if (s2 == NULL)
+	if (s1 != NULL)
+		len_s1 = strlen(s1);
+	if (s2 != NULL)
 		len_s1 = strlen(s1);
 	if (s1 == NULL && s2 == NULL)
 		len_s1 = len_s2 = 0;
