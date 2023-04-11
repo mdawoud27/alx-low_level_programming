25030d702611a3189b1b63f62559b5efb8052bc7
dawoud <mhmaddawoud20@gmail.com>
2023-04-11 09:51:25 -0700

    Adding 3-alloc_grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (0);
-	grid = malloc(height * sizeof(**grid));
+	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (0);
 	for (i = 0; i < height; i++)
