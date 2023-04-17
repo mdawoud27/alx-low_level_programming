a2ca79e93c6471c805b5074a56ec3e01b4398a1e
dawoud <mhmaddawoud20@gmail.com>
2023-04-17 05:58:23 -0700

    Update dog.h  file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
