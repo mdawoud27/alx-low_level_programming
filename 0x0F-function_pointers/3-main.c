d6c09633022bd479386d80f231f3e03e6f2b89ae
dawoud <mhmaddawoud20@gmail.com>
2023-04-19 15:37:37 -0700

    Adding 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv)
 {
 	int n1, n2, res;
 	char *ope;
+	int (*fun_ptr)(int, int);
 
 	if (argc != 4)
 	{
@@ -24,17 +25,18 @@ int main(int argc, char **argv)
 	ope = argv[2];
 	n2 = atoi(argv[3]);
 
-	if (get_op_func(ope) == NULL || ope[1] != '\0')
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && n2 == 0)
+	if ((*ope == '/' || ope == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	res = get_op_func(ope)(n1, n2);
+	fun_ptr = get_op_func(ope);
+	res = fun_ptr(n1, n2);
 
 	printf("%d\n", res);
 	return (0);
