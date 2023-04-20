0e7135f1e87b9c96b8e96b4f909705aa2464e817
dawoud <mhmaddawoud20@gmail.com>
2023-04-20 13:57:46 -0700

    Adding 0-sum_them_all.c file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
+	int sum = 0, i = n;
 	va_list numbers;
 
 	if (n == 0)
 		return (0);
-
 	va_start(numbers, n);
-	for (i = 0; i < n; i++)
+	while (i--)
+	{
 		sum += va_arg(numbers, int);
-	va_end(numbers);
-
+	}
 	return (sum);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,4 +10,40 @@
  */
 void print_all(const char * const format, ...)
 {
+	int i;
+	char *str;
+	va_list all;
 
+	va_start(all, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(all, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(all, double));
+				break;
+			case 'c':
+				printf("%c", va_arg(all, int));
+				break;
+			case 's':
+				str = va_arg(all, char *);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i + 1])
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
