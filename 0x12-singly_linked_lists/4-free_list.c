9d1c4756b75bdfb4d24aca6745ed02e8171c6a86
mdawoud27 <mhmaddawoud20@gmail.com>
2023-04-28 02:32:47 -0700

    Adding 4-free_list.c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: the first node in linked list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+
+	while (head != NULL)
+	{
+		ptr = head->next;
+		free(head->str);
+		free(head);
+		head = ptr;
+	}
+}
