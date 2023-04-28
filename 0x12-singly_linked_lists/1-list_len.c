508d265816b77041936883bd913eea9a00ca08c8
mdawoud27 <mhmaddawoud20@gmail.com>
2023-04-27 23:38:45 -0700

    Adding 1-list_len.c  file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked list_t list.
+ * list_len - function that returns the number of elements
+ * in a linked list_t list.
  * @h: struct node
  *
  * Return: list length or num of nodes
