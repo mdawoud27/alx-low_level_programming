0e74df88604ca740df3449440c42be27a1452c9b
mdawoud27 <mhmaddawoud20@gmail.com>
2023-04-29 09:20:44 -0700

    Upload task's files

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *ptr = head;
 
+	if (head == NULL)
+		return (NULL);
+
 	for (i = 0; i < index; i++)
-	{
-		if (head == NULL)
-			return (NULL);
 		ptr = ptr->next;
-	}
 
 	return (ptr);
 }
