a775accc48b4f366bceb0a2013f6b79df84c39c5
mdawoud27 <mhmaddawoud20@gmail.com>
2023-05-02 13:06:36 -0700

    Update files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,12 +14,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode, *ptr = *head;
 	unsigned int i;
 
-	if (head != NULL)
+	if (head == NULL)
 		return (NULL);
 	ptr = *head;
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
+	{
+		free(newNode);
 		return (NULL);
+	}
 
 	newNode->n = n;
 	if (idx == 0)
@@ -39,4 +42,3 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	return (newNode);
 }
-
