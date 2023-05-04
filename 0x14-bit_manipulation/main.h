40f0bc5e0032ced82a80cc74e948d40e3c8c8c68
mdawoud27 <mhmaddawoud20@gmail.com>
2023-05-04 14:38:19 -0700

    Adding task's files

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,7 +11,7 @@
  * Header file for 0x14. C Bit manipulation
  * ~dawoud
 */
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
