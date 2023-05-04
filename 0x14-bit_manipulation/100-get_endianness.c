510e412c8ea6fecb87c0a35c7bcbcaf93ba55a95
mdawoud27 <mhmaddawoud20@gmail.com>
2023-05-04 16:39:32 -0700

    Upload task's files

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endiannes - function that checks the endianness.
+ * get_endianness - function that checks the endianness.
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
