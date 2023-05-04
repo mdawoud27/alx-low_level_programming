a1a7c235681aa637d5d6d553c62166a5eff20c1a
mdawoud27 <mhmaddawoud20@gmail.com>
2023-05-04 14:55:49 -0700

    Adding task's files

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,22 +3,21 @@
 /**
  * flip_bits - flips m bits
  * @n: decimal number
- * @m: number of bits you would need to flip 
- * 
+ * @m: number of bits you would need to flip
+ *
  * Return: the number of bits you would need to flip to get from
  * one number to another.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int number, i;
+	unsigned long int number, i;
 
-    number = n ^ m;
-    while (number >= 1)
-    {
-        if (number & 1 == 1)
-            i++;
-        number >>= 1;
-    }
-
-    return (i)l
+	number = n ^ m;
+	while (number >= 1)
+	{
+		if (number & 1 == 1)
+			i++;
+		number >>= 1;
+	}
+	return (i);
 }
