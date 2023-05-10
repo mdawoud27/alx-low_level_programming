fe8aa942b24604f6e71232f5318d2fdab5224b9e
mdawoud27 <mhmaddawoud20@gmail.com>
2023-05-10 01:34:36 -0700

    Adding 0-read_textfile.c file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,11 +7,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/**
- * Header file for 0x15-file_io task
- * ~dawoud
- */
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
