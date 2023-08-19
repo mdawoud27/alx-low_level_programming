9feaed523b2600296f9a01a849a78dd69bbb9a74
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-19 04:56:58 -0700

    Adding free file

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list.
+ * @head: head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head)
+	{
+		dlistint_t *temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
