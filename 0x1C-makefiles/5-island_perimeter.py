bc1db4b525aafd79f85aa6b627007e9b2b31c953
dawoud <mhmaddawoud20@gmail.com>
2023-10-27 10:23:54 +0200

    Adding 5-island_perimeter.py file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Function returns the perimeter of the island
+    described in grid
+    """
+    row = len(grid)
+    col = len(grid[0])
+    perimeter = 0
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j]:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j]:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1]:
+                    perimeter -= 2
+        return perimeter
