b97caedca79cef35391949fb9b74075df729cdb5
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-06 20:35:03 +0200

    Adding interpolation search function

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -31,5 +31,8 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			low = pos + 1;
 	}
+	pos = low + (((double)(high - low) / (array[high] - array[low])) *
+								(value - array[low]));
 	printf("Value checked array[%ld] is out of range\n", pos);
 	return (-1);
+}
